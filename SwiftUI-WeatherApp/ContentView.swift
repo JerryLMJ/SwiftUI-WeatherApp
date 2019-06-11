87c29fbe674d7a4437c3affd0a0e77c72189b697
JerryLMJ <jerrylmj@JerryLMJdeMacBook-Pro.local>
2019-06-11 14:33:55 +0800

    init project

---
+++ SwiftUI-WeatherApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUI-WeatherApp
+//
+//  Created by JerryLMJ on 2019/6/11.
+//  Copyright © 2019 JerryLMJ. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
